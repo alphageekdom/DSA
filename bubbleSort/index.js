10bcf0cc243ebaa93733aa9b2f2478ba4fa76692
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2022-10-27 17:43:11 -0700

    Update

--- bubbleSort/index.js
+++ bubbleSort/index.js
@@ -1,37 +1,46 @@
-// Sort 1 step
+// Bubble Sort in 1 step
 const bubbleSort1 = arr => {
+    let steps = 0;
     arr.sort((a, b) => a - b);
+    steps++;
+
+    console.log(`Steps: ${steps}`)
     return arr;
 };
 
-// For Loop in 21 steps
+// Bubble Sort in 30 steps
 const bubbleSort2 = arr => {
     const len = arr.length;
+    let steps = 0;
     // Iterate the array
     for (let i = 0; i < len; i++) {
         // Compare each value
+        steps++;
         for (let j = 0; j < len; j++) {
+            steps++;
             if (arr[j] > arr[j + 1]) {
-                console.log('Steps')
                 const temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
         }
     }
+    console.log(`Steps: ${steps}`)
     return arr;
 };
 
-// While loop bubble sort in 21 sets
+// Bubble Sort in 20 steps
 const bubbleSort3 = arr => {
     let unsortedIndex = arr.length - 1;
     let sorted = false;
+    let steps = 0;
 
     while (!sorted) {
+        steps++;
         sorted = true;
         for (let i = 0; i < unsortedIndex; i++) {
+            steps++;
             if (arr[i] > arr[i +1]) {
-                console.log('Steps')
                 let temp = arr[i];
                 arr[i] = arr[i + 1];
                 arr[i + 1] = temp;
@@ -39,10 +48,11 @@ const bubbleSort3 = arr => {
             }
         }
     }
+    console.log(`Steps: ${steps}`)
     return arr;
 };
 
 
-console.log(bubbleSort1([65, 55, 45, 35, 25, 15, 10]));
-console.log(bubbleSort2([65, 55, 45, 35, 25, 15, 10]));
-console.log(bubbleSort3([65, 55, 45, 35, 25, 15, 10]));
+console.log(bubbleSort1([11, 33, 53, 3, 2]));
+console.log(bubbleSort2([65, 55, 45, 35, 25]));
+console.log(bubbleSort3([4, 2, 7, 1, 3]));
---
+++ selectionSort/app.js
@@ -0,0 +1,29 @@
+const selectionSort = arr => {
+
+    let steps = 0;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        let lowestNumberIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            steps++;
+            if (arr[j] < arr[lowestNumberIndex]) {
+                lowestNumberIndex = j;
+                steps++;
+            }
+        }
+        if (lowestNumberIndex != i) {
+            let temp = arr[i];
+            arr[i] = arr[lowestNumberIndex];
+            arr[lowestNumberIndex] = temp;
+        }
+    }
+
+    console.log(`Steps: ${steps}`)
+    return arr
+};
+
+
+
+
+
+console.log(selectionSort([4, 2, 7, 1, 3]))
